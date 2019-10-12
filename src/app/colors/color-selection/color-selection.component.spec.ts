1ed643b94f8ceb502c00a354991c037213ea0131
Hodossy, Szabolcs <hodossy.szabolcs@gmail.com>
2019-10-12 18:10:05 +0200

    Added color selection

---
+++ src/app/colors/color-selection/color-selection.component.spec.ts
@@ -0,0 +1,40 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ColorSelectionComponent } from './color-selection.component';
+
+describe('ColorSelectionComponent', () => {
+  let component: ColorSelectionComponent;
+  let fixture: ComponentFixture<ColorSelectionComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ColorSelectionComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ColorSelectionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should allow a new color placeholder', () => {
+    expect(component.canAdd()).toBeTruthy();
+  });
+
+  it('should not allow more color placeholders', () => {
+    component.addNewColor();
+    expect(component.canAdd()).toBeFalsy();
+  });
+
+  it('should add a new color placeholder', () => {
+    component.addNewColor();
+    expect(component.colors.length).toEqual(1);
+  });
+
+});
---
+++ src/app/colors/color-selection/color-selection.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input } from '@angular/core';
+
+export interface ColorChoice {
+  color: string;
+  selected: boolean;
+}
+
+@Component({
+  selector: 'app-color-selection',
+  templateUrl: './color-selection.component.html',
+  styleUrls: ['./color-selection.component.scss']
+})
+export class ColorSelectionComponent {
+  @Input() colors: Array<ColorChoice> = [];
+
+  addNewColor(): void {
+    if(this.canAdd()) {
+      this.colors.push({ color: undefined, selected: false });
+    }
+  }
+
+  canAdd(): boolean {
+    return this.colors.length === 0 || this.colors[this.colors.length - 1].color != undefined;
+  }
+
+  openChooseColor() {
+    throw Error('Color selection is not implemented yet.');
+  }
+
+}
