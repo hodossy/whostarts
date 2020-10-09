78525324ac8ae2790c338e8bb23161c41bbfe0d9
Hodossy, Szabolcs <szabolcs.hodossy@continental-corporation.com>
2020-10-09 14:25:27 +0200

    chore: prepare tag based releases

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { SettingsService } from './settings.service';
 
+import { environment } from '../../environments/environment';
+
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
+  version = environment.version;
 
   constructor(
     public settings: SettingsService
