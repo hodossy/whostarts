ccd61d0e8ccad7e38caa93bbcf821c1c9e45a3dd
Hodossy, Szabolcs <szabolcs.hodossy@continental-corporation.com>
2020-10-13 09:18:20 +0200

    chore: extract version in a separate step

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ import { version } from '../../package.json';
 
 export const environment = {
   production: true,
-  version: version.startsWith('refs/tags/') ? version.substr(10) : version
+  version
 };
